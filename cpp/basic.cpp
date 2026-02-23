14733cebf59f5e3becc3c64a84004c432f1c6dde
Ayush Belwal <ayushbelwal261@gmail.com>
2026-02-23 11:43:30 +0530

    basic

---
+++ cpp/basic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+// Function defination
+int printhello(){
+    cout << "Hello"<<endl;
+    return 3;
+}
+int main(){
+    printhello();
+    return 0;
+}
+
